3d0fc66aa334624b97d895169e9b8ca9d27f32c0
У Цзюньфэй <90907656+br1kfast@users.noreply.github.com>
2023-02-22 07:35:26 +0300

    Delete payment-service directory

--- payment-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE payment
-(
-    id          SERIAL PRIMARY KEY,
-    paymentUid uuid        NOT NULL,
-    status      VARCHAR(20) NOT NULL
-        CHECK (status IN ('PAID', 'CANCELED')),
-    price       INT         NOT NULL
-);
