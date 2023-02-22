b11e403a5bc070c9f535b9a213f2b3ed9d7c64f8
У Цзюньфэй <90907656+br1kfast@users.noreply.github.com>
2023-02-22 07:34:27 +0300

    Delete loyalty-service directory

--- loyalty-service/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO loyalty(id,username,reservationCount,status,discount) VALUES (1,'Test Max',25,'GOLD',10);
--- loyalty-service/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE loyalty
-(
-    id                SERIAL PRIMARY KEY,
-    username          VARCHAR(80) NOT NULL UNIQUE,
-    reservationCount INT         NOT NULL DEFAULT 0,
-    status            VARCHAR(80) NOT NULL DEFAULT 'BRONZE'
-        CHECK (status IN ('BRONZE', 'SILVER', 'GOLD')),
-    discount          INT         NOT NULL
-);
